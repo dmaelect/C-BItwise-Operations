d1d90fd4898af16eb39efa211c95be7f53b590d5
dmaelect <daryl@dmaelectronics.com>
2017-07-06 14:33:45 -0600

    Update ToUpperToLower.c

--- Bitwise/To Upper To Lower/ToUpperToLower.c
+++ Bitwise/To Upper To Lower/ToUpperToLower.c
@@ -1,6 +1,12 @@
  /*
  Date:         07/06/17
- Description:  Use Bitwise & and | to convert charactors to upper and lower case
+ Description:  Use Bitwise & and | to convert charactors to upper and lower case.
+ One function uses array notation to iterate the string array and the other function uses pointer notation.
+ 
+ This works because the hex value for lower case letters and upper case letters are the same except for bit 5
+ If bit 5 is 1 then the letter is lower case. We then mask the 8 bit letter with 0010 0000 by OR-ing it. This turns on bit 5
+ If bit 5 is 0 then the letter is upper case. We then unmask the 8 bit letter with 1101 1111 by AND-ing it. This turns off bit 5
+ 
  Source:       Daryl Newell
  */
 
