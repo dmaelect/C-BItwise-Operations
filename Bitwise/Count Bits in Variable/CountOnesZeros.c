c18ae95e2fc406f58000c761b5d6d0a1f78acff3
dmaelect <daryl@dmaelectronics.com>
2018-01-19 17:41:00 -0700

    Update CountOnesZeros.c

--- Bitwise/Count Bits in Variable/CountOnesZeros.c
+++ Bitwise/Count Bits in Variable/CountOnesZeros.c
@@ -19,9 +19,9 @@ Source       Me
                                      //-- it is not required to do it but I prefer it.
 //---------------------------------Macros-------------------------------
 #ifndef UINT
-#define UINT unsigned int
-#define BYTE 8
-#define BIT_SIZE32 (sizeof(UINT) * BYTE)
+#define UINT unsigned int           //-- define an unsigned int
+#define BYTE 8                      //-- define a byte size
+#define BIT_SIZE32 (sizeof(UINT) * BYTE)   //-- easier to type BIT_SIZE32
 #endif
 //--------------------------------Functions------------------------------------
 int CountBits(UINT _n, UINT *_nOnes, UINT *_nZeros);
@@ -29,10 +29,10 @@ int CountBits(UINT _n, UINT *_nOnes, UINT *_nZeros);
 int main(int argc, char* argv[])
 {
 	int test = 0;
-	UINT zeros = 0, ones = 0;
-	UINT i = 0, x = 0xFFFAFFFF;
-    UINT bitMask = (1 << (BIT_SIZE32 - 1));   //-- move the 1 mask to the MSB of a 32 bit #
-	int y = 0xFFFAFFFF;
+	UINT zeros = 0, ones = 0;                 //-- the sum of ones and zeros, does not need to be UINT
+	UINT i = 0, x = 0xFFFAFFFF;               //-- test input vars
+        UINT bitMask = (1 << (BIT_SIZE32 - 1));   //-- move the 1 mask to the MSB of a 32 bit #
+	int y = 0xFFFAFFFF;                       //-- Test input vars
 
 //	printBinary(y);
 
@@ -49,9 +49,9 @@ int main(int argc, char* argv[])
 int CountBits(UINT number, UINT *_ones, UINT *_zeros)
 {
 	UINT i = 0;
-	UINT bitMask = 1 << (BIT_SIZE32 - 1);   //-- move the 1 mask to the MSB of a 32 bit #
+	UINT bitMask = 1 << (BIT_SIZE32 - 1);       //-- move the 1 mask to the MSB of a 32 bit #
 
-	printBinary(number);
+	printBinary(number);                        //-- Display the binary bit pattern
 
 	for(i = 0; i < BIT_SIZE32; ++i)
 	{
@@ -60,11 +60,11 @@ int CountBits(UINT number, UINT *_ones, UINT *_zeros)
 //		printBinary(bitMask);
 ---------------------------    End Testing only  ------------------------------------*/
     if(number & bitMask) {
-			*_ones += 1;
+			*_ones += 1;                 //-- Add up the "ones"
 		}
 
 		if((number & bitMask) == 0){
-			*_zeros += 1;
+			*_zeros += 1;                //-- Add up the "zeros"
 		}
 		bitMask >>= 1;                       //-- move the mask bit to the right on 
 		                                     //-- each loop iteration to test the next bit
